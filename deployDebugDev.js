6af1b648bab48f8da0ddbfb4c54be7dd3d318417
tsaltd <wp710012@gmail.com>
2019-07-11 16:48:26 -0400

    Update deployDebugDev.js

--- deployDebugDev.js
+++ deployDebugDev.js
@@ -10,39 +10,36 @@ const web3 = createWeb3();
 const sender = '0xe7fbecfe55d0ac37c4261db41a97d56f5a72a817';
 // eslint-disable-next-line no-use-before-define
 deployContract(web3, contract, sender)
-    .then(() => {
-        // eslint-disable-next-line no-console
-        console.log('Deployment finished');
+    .then(function () {
+        console.log('Deployment finished')
     })
-    .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.log(`Failed to deploy contract: ${JSON.stringify(err)}`);
+    .catch(error => {
+        console.log(`Failed to deploy contract: ${Jason.stringify(error)}`);
     });
-
 function compileContract() {
     const compilerInput = {
         language: 'Solidity',
         sources: {
             'Voter.sol': {
-                content: fs.readFileSync('Voter.sol', 'utf8'),
-            },
+                content: fs.readFileSync('Voter.sol', 'utf8')
+            }
         },
         settings: {
             outputSelection: {
                 '*': {
-                    '*': ['*'],
-                },
-            },
-        },
+                    '*': ['*']
+                }
+            }
+        }
     };
 
     // eslint-disable-next-line no-console
-    console.log('Compiling the contract');
+    console.log('Compiling the contract')
     // Compile the contract
     const output = JSON.parse(solc.compile(JSON.stringify(compilerInput)));
     // Get compiled contract
     // eslint-disable-next-line no-shadow
-    const contract = output.contracts['Voter.sol'].Voter;
+    const contract = output.contracts['Voter.sol']['Voter']
 
     // Save contract's ABI
     const { abi } = contract.abi;
@@ -54,43 +51,35 @@ function compileContract() {
 function createWeb3() {
     return new Web3('http://127.0.0.1:8545');
 }
-// function createWeb3() {
-// let web3 = new Web3();
-// web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
-
-// return web3;
-// }
 
 async function deployContract(web3, contract, sender) {
     const Voter = new web3.eth.Contract(contract.abi);
-    const bytecode = `0x${contract.evm.bytecode.object}`;
+    const bytecode = '0x' + contract.evm.bytecode.object ;
     const gasEstimate = await web3.eth.estimateGas({ data: bytecode });
 
     // eslint-disable-next-line no-console
     console.log('Deploying the contract');
     // eslint-disable-next-line no-console
     console.log(gasEstimate);
-    const contractInstance = await Voter.deploy({
-        data: bytecode,
+    let contractInstance = await Voter.deploy({
+        data: bytecode
     })
         .send({
             from: sender,
-            gas: gasEstimate,
+            gas: gasEstimate
             // gas: 1890202
         })
-        .on('transactionHash', (transactionHash) => {
+        .on('transactionHash', function (transactionHash)  {
             console.log(`Transaction hash: ${transactionHash}`);
         })
         //.on('confirmation', (confirmationNumber, receipt) 
-        .on('confirmation', confirmationNumber => {
+        .on( `confirmation`, function(confirmationNumber, reciept)  {
             console.log(`Confirmation number: ${confirmationNumber}`);
         })
-        .on('error', error => {
-          //  console.log('Transaction Error: ${error.message}');
-            console.log(`Transaction Error: ${JSON.stringify(error)}`);
-        });
+
+        .on("error", error => {
+            console.log(`Transaction Error: ${error.message}`);
+        })
 
     console.log(`Contract address: ${contractInstance.options.address}`);
 }
-
-
